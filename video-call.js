e52fa281136e01d01d01c13703258fa16706862c
Izzah <l201314@lhr.nu.edu.pk>
2023-05-15 13:33:23 +0500

    Setting up Video call server

--- video-call.js
+++ video-call.js
@@ -37,4 +37,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => console.log("server is running on port 5000"));
+server.listen(5000, '0.0.0.0', () => console.log("server is running on port 5000"));
